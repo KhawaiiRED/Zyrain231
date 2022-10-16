121f53ee2ba7e78f92edeff553c6295c60a61829
KhawaiiRED <103673156+KhawaiiRED@users.noreply.github.com>
2022-10-16 13:50:58 +0800

    Create hatch.lua

---
+++ hatch.lua
@@ -0,0 +1,5 @@
+	_G.autotap1 = true;
+	while _G.autotap1 == true do
+	local args = {[1] = {[1] = "Rainbow Pixel Egg",[2] = false}}workspace:FindFirstChild("CHECKMATE ROBLOX").__REMOTES:FindFirstChild("buyx egg"):InvokeServer(unpack(args))
+	wait()
+	end
